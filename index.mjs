ddbb534158e37cdbede2147c78cdba84d87a9a02
Howard Stearns <howard.stearns@gmail.com>
2025-01-11 11:36:42 -0800

    one web component per file

--- index.mjs
+++ index.mjs
@@ -1,4 +1,34 @@
-export * from './components.mjs';
+import '@material/web/all.js';
+export { App, MDElement } from './lib/md-element.mjs';
+
+export { MaterialIcon } from './lib/material-icon.mjs';
+export { AppQrcode } from './lib/app-qrcode.mjs';
+export { AvatarJdenticon } from './lib/app-jdenticon.mjs';
+export { ListDivider } from './lib/list-divider.mjs';
+export { SecurityQuestionSelection } from './lib/security-question-selection.mjs';
+
+export { ViewTransform } from './lib/view-transform.mjs';
+export { ListItem } from './lib/list-item.mjs';
+export { MenuItem } from './lib/menu-item.mjs';
+export { TabItem } from './lib/tab-item.mjs';
+
+export { ListTransform } from './lib/list-transform.mjs';
+export { ListItems } from './lib/list-items.mjs';
+export { MenuButton } from './lib/menu-button.mjs';
+export { MenuTabs } from './lib/menu-tabs.mjs';
+
+export { ChooserButton } from './lib/chooser-button.mjs';
+export { UserChooser } from './lib/user-chooser.mjs';
+
+export { ChoiceAmongLocallyStoredOptions } from './lib/choice-among-locally-stored-options.mjs';
+
+export { SwitchUser } from './lib/switch-user.mjs';
+export { UserProfile } from './lib/user-profile.mjs';
+export { AppShare } from './lib/app-share.mjs';
+export { AppFirstuse } from './lib/app-firstuse.mjs';
+export { BasicApp } from './lib/basic-app.mjs';
+
+
 
 
 
