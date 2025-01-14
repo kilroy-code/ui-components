aef8780108ec65ea4a162911e47d403d248d7c16
Howard Stearns <howard.stearns@gmail.com>
2025-01-13 17:19:40 -0800

    start to refactor transformer management

--- index.mjs
+++ index.mjs
@@ -7,6 +7,9 @@ export { AvatarJdenticon } from './lib/app-jdenticon.mjs';
 export { ListDivider } from './lib/list-divider.mjs';
 export { SecurityQuestionSelection } from './lib/security-question-selection.mjs';
 
+export { MutableCollection, LiveRecord } from './lib/mutable-collection.mjs';
+export { CollectionTransform } from './lib/collection-transform.mjs';
+
 export { ViewTransform } from './lib/view-transform.mjs';
 export { ListItem } from './lib/list-item.mjs';
 export { MenuItem } from './lib/menu-item.mjs';
@@ -14,7 +17,7 @@ export { TabItem } from './lib/tab-item.mjs';
 
 export { ListTransform } from './lib/list-transform.mjs';
 export { ListItems } from './lib/list-items.mjs';
-export { MenuButton } from './lib/menu-button.mjs';
+export { FixmeMenuButton } from './lib/menu-button.mjs';
 export { MenuTabs } from './lib/menu-tabs.mjs';
 
 export { ChooserButton } from './lib/chooser-button.mjs';
