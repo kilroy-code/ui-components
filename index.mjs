aeab4e3add10463bbf354a0250f2eb42c08149f2
Howard Stearns <howard.stearns@gmail.com>
2025-01-14 11:43:30 -0800

    remove cruft

--- index.mjs
+++ index.mjs
@@ -9,20 +9,16 @@ export { SecurityQuestionSelection } from './lib/security-question-selection.mjs
 
 export { MutableCollection, LiveRecord } from './lib/mutable-collection.mjs';
 export { CollectionTransform } from './lib/collection-transform.mjs';
-
 export { BaseTransformer } from './lib/base-transformer.mjs';
+
 export { ViewTransform } from './lib/view-transform.mjs'; // fixme remove
 export { ListItem } from './lib/list-item.mjs';
-export { MenuItem } from './lib/menu-item.mjs';
 export { TabItem } from './lib/tab-item.mjs';
 
 export { ListTransform } from './lib/list-transform.mjs';
 export { ListItems } from './lib/list-items.mjs';
-export { MenuButton, MenuTransformer/*, FixmeMenuButton*/ } from './lib/menu-button.mjs';
+export { MenuButton, MenuTransformer } from './lib/menu-button.mjs';
 export { MenuTabs } from './lib/menu-tabs.mjs';
-
-//export { ChooserButton } from './lib/chooser-button.mjs';
-//export { UserChooser } from './lib/user-chooser.mjs';
 
 export { ChoiceAmongLocallyStoredOptions } from './lib/choice-among-locally-stored-options.mjs';
 
